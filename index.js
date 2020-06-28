f99618c6b6001c99a5ed01d931ab4d3468c79e62
keeinw2542 <60432929+keeinw2542@users.noreply.github.com>
2020-06-28 23:39:06 +0700

    add /data

--- index.js
+++ index.js
@@ -6,81 +6,81 @@ var bodyParser = require('body-parser')
 
 require("dotenv").config()
 //CREAT FUNTION TO RANDOM SALT
-var genRandomString = function(length){
-    return crypto.randomBytes(Math.ceil(length/2))
+var genRandomString = function (length) {
+    return crypto.randomBytes(Math.ceil(length / 2))
         .toString('hex') //convert to hex format
-        .slice(0,length);
-    };
+        .slice(0, length);
+};
 
-var sha512 = function(password,salt){
-    var hash =crypto.createHmac('sha512',salt);
+var sha512 = function (password, salt) {
+    var hash = crypto.createHmac('sha512', salt);
     hash.update(password);
     var value = hash.digest('hex');
     return {
-        salt:salt,
-        passwordHash:value
+        salt: salt,
+        passwordHash: value
     };
 };
 
 function saltHashPassword(userPassword) {
     var salt = genRandomString(16);
-    var passwordData =sha512(userPassword,salt);
+    var passwordData = sha512(userPassword, salt);
     return passwordData;
 }
 
-function checkHashPassword(userPassword,salt) {
-    var passwordData = sha512(userPassword,salt);
+function checkHashPassword(userPassword, salt) {
+    var passwordData = sha512(userPassword, salt);
     return passwordData;
 }
 
 //Create Express Service
-var app =express();
+var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Create Express Service
 var MongoClient = mongodb.MongoClient;
 
 //Connection URL
-var url =process.env.MONGO_URL
+var url = process.env.MONGO_URL
 //var url = 'mongodb://localhost:27017' //27017 is default or port
 
-MongoClient.connect(url,{useNewUrlParser: true},function(err,client){
+MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
     if (err) {
-        console.log('Unable to connect to the mongoDB server. ERROR',err);
+        console.log('Unable to connect to the mongoDB server. ERROR', err);
     } else {
         //Register
-        app.post('/register',(request,response,next)=>{
+        app.post('/register', (request, response, next) => {
             var post_data = request.body;
 
             var plaint_password = post_data.password;
             var hash_data = saltHashPassword(plaint_password);
 
-            var password =hash_data.passwordHash; //Save password hash
-            var salt =hash_data.salt; //save salt
+            var password = hash_data.passwordHash; //Save password hash
+            var salt = hash_data.salt; //save salt
 
-            var name =post_data.name;
-            var email =post_data.email;
+            var name = post_data.name;
+            var email = post_data.email;
 
-            var insertJson= {
-                'email':email,
-                'password':password,
-                'salt':salt,
-                'name':name
+            var insertJson = {
+                'email': email,
+                'password': password,
+                'salt': salt,
+                'name': name
             };
             var db = client.db('keetest');
 
             //Check exist email
             db.collection('user')
-                .find({'email':email}).count(function(err,number){
-                    if(number != 0){
+                .find({ 'email': email }).count(function (err, number) {
+                    if (number != 0) {
                         response.json('email alrady exists');
                         console.log('email alrady exists');
                     }
-                    else{
+                    else {
                         //Insert data 
                         db.collection('user')
-                            .insertOne(insertJson,function(err,res){
+                            .insertOne(insertJson, function (err, res) {
                                 response.json('Registration success');
                                 console.log('Registration suscess');
                             })
@@ -89,46 +89,75 @@ MongoClient.connect(url,{useNewUrlParser: true},function(err,client){
 
         })
 
-        app.post('/login',(request,response,next)=>{
+        app.post('/login', (request, response, next) => {
             var post_data = request.body;
 
-            var email =post_data.email;
-            var userPassword =post_data.password;
-            
+            var email = post_data.email;
+            var userPassword = post_data.password;
+
             var db = client.db('keetest');
 
             //Check exist email
             db.collection('user')
-                .find({'email':email}).count(function(err,number){
-                    if(number == 0){
+                .find({ 'email': email }).count(function (err, number) {
+                    if (number == 0) {
                         response.json('email not exists');
                         console.log('email not exists');
                     }
-                    else{
+                    else {
                         //Insert data 
                         db.collection('user')
-                            .findOne({'email':email},function(err,user){
-                                var salt =user.salt; //get salt from user
-                                var hashed_password = checkHashPassword(userPassword,salt).passwordHash;//hash password with salt
+                            .findOne({ 'email': email }, function (err, user) {
+                                var salt = user.salt; //get salt from user
+                                var hashed_password = checkHashPassword(userPassword, salt).passwordHash;//hash password with salt
                                 var encrypted_password = user.password;//getpassword from user
-                                if (hashed_password ==encrypted_password){
+                                if (hashed_password == encrypted_password) {
                                     response.json('Login success');
                                     console.log('Login suscess');
-                                    
+
                                 }
                                 else {
                                     response.json('worng password');
                                     console.log('worng password');
-                                    
+
                                 }
                             })
                     }
                 })
 
         })
+        app.post('/data', (request, response, next) => {
+            var post_data = request.body;
 
+            var username = post_data.username;
+            var numberfile = 0;
+            var info1 = post_data.info1;
+            var info2 = post_data.info2;
+            var info3 = post_data.info3;
+
+
+            var db = client.db('keetest');
+
+            //Check exist email
+            db.collection('keeinw')
+                .count(function (err, number) {
+                    numberfile = number
+                })
+            var insertJson = {
+                'numberfile': numberfile,
+                'info1': info1,
+                'info1': info2,
+                'info1': info3
+            };
+            //Insert data 
+            db.collection('keeinw')
+                .insertOne(insertJson, function (err, res) {
+                    response.json('Add success');
+                    console.log('Add suscess');
+                })
+        })
         //Start Web Server
-        app.listen(process.env.PORT||3000,()=>{
+        app.listen(process.env.PORT || 3000, () => {
             console.log('Connected to mongoDB Server ,Webservice is runing');
         })
     }
